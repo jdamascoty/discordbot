cdd5235de9bb5327a4ed39e56fdf777f2be5017c
jdamascoty <jessicadamascoty@gmail.com>
2021-02-26 09:48:04 -0500

    Update create_dataset to include fb data

--- nlp/create_dataset.py
+++ nlp/create_dataset.py
@@ -2,7 +2,10 @@ import random
 import re
 import torch
 import json
-import glob
+import os
+
+FACEBOOK_NAME = ""
+DISCORD_AUTHOR_ID = ""
 
 def tokenize(dataset, epochs):
 	total_text = '<|endoftext|>' #token
@@ -11,21 +14,34 @@ def tokenize(dataset, epochs):
 		random.shuffle(tweets)
 		total_text += '<|endoftext|>'.join(tweets) + '<|endoftext|>'
 
+	print(len(total_text))
 	return total_text
 
-def create_dataset(user_id, files, train_dataset_size=0.9):
+def create_dataset(fb_name, discord_id, files, train_dataset_size=0.9):
 	user_text = []
-	for f in files:
-		print(f)
-		file_data = open(f, encoding="utf8")
+	for source in files:
+		filenames = files[source]
+		for file in filenames:
+			file_data = open("./{}/{}".format(source, file), encoding="utf8")
+			data = json.load(file_data)
+			messages = data["messages"]
 
-		data = json.load(file_data)
-
-		messages = data["messages"]
-
-		for m in messages:
-			if(m["author"]["id"] == user_id):
-				user_text.append(m["content"])
+			for m in messages:
+				try:
+					if source == "fb":
+						if m["sender_name"] == fb_name:
+							try:
+								user_text.append(m["content"])
+							except:
+								pass
+					elif source == "discord":
+						if m["author"]["id"] == discord_id:
+							try:
+								user_text.append(m["content"])
+							except:
+								pass
+				except:
+					pass
 
 	random.shuffle(user_text)
 	train_size = int(train_dataset_size * len(user_text))
@@ -34,15 +50,22 @@ def create_dataset(user_id, files, train_dataset_size=0.9):
 
 	EPOCHS = 4
 
-	with open('{}_train.txt'.format(user_id), 'w', encoding="utf8") as f:
+	with open('{}_train.txt'.format(discord_id), 'w', encoding="utf8") as f:
 		data = tokenize(train_dataset, EPOCHS)
 		f.write(data)
 
-	with open('{}_valid.txt'.format(user_id), 'w', encoding="utf8") as f:
+	with open('{}_valid.txt'.format(discord_id), 'w', encoding="utf8") as f:
 		data = tokenize(valid_dataset, 1)
 		f.write(data)
 
 if __name__ == "__main__":
-	user_id = "" #insert discord id here
-	files = glob.glob("*.json")
-	create_dataset(user_id, files)
+	fb_name = FACEBOOK_NAME
+	discord_id = DISCORD_AUTHOR_ID
+
+	fb_files = [f for f in os.listdir("./fb/") if f.endswith('.json')]
+	discord_files = [f for f in os.listdir("./discord/") if f.endswith('.json')]
+
+	files = {}
+	files["fb"] = fb_files
+	files["discord"] = discord_files
+	create_dataset(fb_name, discord_id, files)
